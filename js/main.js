afaccf677da538db4116619303cbfb1f8c2bd7f4
Luis Angel Santos <santosangel748@gmail.com>
2022-12-11 13:10:51 -0600

    Agregando proyecto FISIO y formulario de contacto

--- js/main.js
+++ js/main.js
@@ -39,3 +39,15 @@ function efectoHabilidades(){
 window.onscroll = function(){
     efectoHabilidades();
 } 
+
+function contacto(){
+    var nombre = document.getElementsByClassName('datosContacto')[0].value;
+    var telefono = document.getElementsByClassName('datosContacto')[1].value;
+    var correo = document.getElementsByClassName('datosContacto')[2].value;
+    var tema = document.getElementsByClassName('datosContacto')[3].value;
+    var mensaje = document.getElementsByClassName('datosContacto')[4].value;
+
+    window.location.href = `mailto:santosangel748@gmail.com?=Subject=${tema}$body=Hola%20que%20tal%20soy${nombre},%20me%gustaria%20decirte%20lo%20siguiente%20%${mensaje}.
+                            %20Aquí%20te%20dejo%20mis%20datos%20de%20contacto:%20%Telfono%20${telefono}%20y%20%correo%20${correo}.
+                            Un%20saludo,%20%${nombre}`
+}
