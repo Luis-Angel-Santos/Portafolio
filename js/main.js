5799eedc349509e355a9732db8e9abc846373feb
Luis Angel Santos <santosangel748@gmail.com>
2022-12-11 13:22:16 -0600

    Solucion bug contacto final

--- js/main.js
+++ js/main.js
@@ -47,7 +47,7 @@ function contacto(){
     var tema = document.getElementsByClassName('datosContacto')[3].value;
     var mensaje = document.getElementsByClassName('datosContacto')[4].value;
 
-    window.location.href = `mailto:santosangel748@gmail.com?=Subject=${tema}&body=Hola%20que%20tal%20soy${nombre},%20me%gustaria%20decirte%20lo%20siguiente%20%${mensaje}.
-                            %20Aquí%20te%20dejo%20mis%20datos%20de%20contacto:%20%Telfono%20${telefono}%20y%20%correo%20${correo}.
-                            Un%20saludo,%20%${nombre}`
+    window.location.href = `mailto:santosangel748@gmail.com?Subject=${tema}&body=Hola%20que%20tal%20soy%20${nombre},%20me%20gustaria%20decirte%20lo%20siguiente%20${mensaje}.
+        %20Aquí%20te%20dejo%20mis%20datos%20de%20contacto:%20Telefono-->%20${telefono}%20y%20Correo-->%20${correo}.
+        Un%20saludo,%20${nombre}`
 }
