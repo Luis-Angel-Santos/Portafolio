8b485c50b534b6301d38034af833039d6575ce5f
Angel Santos <santosangel748@gmail.com>
2025-11-04 18:23:09 -0600

    Se actualizan datos - Optimizaciones y mejoras visuales

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 let menuVisible = false;
 
+const frases = [
+  "un apasionado por el código limpio.",
+  "un desarrollador enfocado en la eficiencia.",
+  "un amante de la innovación y la tecnología.",
+  "un profesional que busca mejorar cada día.",
+  "un fan de los retos y los nuevos aprendizajes.",
+  "alguien que combina tecnología con visión.",
+  "alguien motivado por crear experiencias útiles.",
+  "un programador que disfruta cada línea bien escrita.",
+  "un entusiasta de la arquitectura y el buen diseño.",
+];
+
+
+const typingElement = document.querySelector(".typing");
+let fraseIndex = 0;
+let letraIndex = 0;
+let borrando = false;
+
 const datosPersonales = [
     { label: 'Cumpleaños', value: '09 de febrero' },
     { label: 'Teléfono', value: '241 239 4657' },
-    { label: 'Email', value: 'santosangel748@gmail.com' },
+    { label: 'Internacional', value: '+52 1 241 239 4657' },
+    { label: 'Email', value: 's.angeldev@gmail.com' },
     { label: 'Dirección', value: 'Tlaxcala, México' },
-    { label: 'Cargo Actual', value: 'Desarrollador Java' },
+    { label: 'Cargo Actual', value: 'Desarrollador Web' },
 ];
 
 const intereses = [
@@ -30,56 +49,32 @@ const habilidadesTecnicas = [
     { icon: 'mysql', title: 'MySQL' },
     { icon: 'mongodb', title: 'MongoDB' },
     { icon: 'firebase', title: 'Firebase' },
+    { icon: 'firebird', title: 'Firebird' },
+    { icon: 'android', title: 'Android' },
+    { icon: 'postman', title: 'Postman' },
+    { icon: 'express', title: 'Express' },
     { icon: 'dotnetcore', title: '.NET Core' },
     { icon: 'bootstrap', title: 'Bootstrap' },
     { icon: 'bash', title: 'Bash' },
     { icon: 'git', title: 'Git' },
     { icon: 'slack', title: 'Slack' },
+    { icon: 'vscode', title: 'VS Code' },
 ];
 
 const habilidadesBlandas = [
-    { icon: 'fa-regular fa-face-smile', title: 'Comunicación' },
-    { icon: 'fa-solid fa-people-group', title: 'Trabajo en Equipo' },
-    { icon: 'fa-solid fa-star', title: 'Creatividad' },
-    { icon: 'fa-solid fa-hands-clapping', title: 'Dedicación' },
-    { icon: 'fa-solid fa-bars-progress', title: 'Administración de proyectos' },
-    { icon: 'fa-solid fa-arrow-up-right-dots', title: 'Proactividad' },
-    { icon: 'fa-solid fa-people-arrows', title: 'Adaptabilidad' },
-    { icon: 'fa-solid fa-tools', title: 'Resolución de problemas' },
-    { icon: 'fa-solid fa-calendar', title: 'Organización' },
+  { icon: 'fa-solid fa-comments', title: 'Comunicación Efectiva' },
+  { icon: 'fa-solid fa-people-group', title: 'Trabajo en Equipo' },
+  { icon: 'fa-solid fa-people-arrows', title: 'Adaptabilidad' },
+  { icon: 'fa-solid fa-lightbulb', title: 'Pensamiento Crítico' },
+  { icon: 'fa-solid fa-brain', title: 'Resolución de Problemas' },
+  { icon: 'fa-solid fa-arrow-up-right-dots', title: 'Proactividad' },
+  { icon: 'fa-solid fa-clock', title: 'Gestión del Tiempo' },
+  { icon: 'fa-solid fa-bullseye', title: 'Orientación a Resultados' },
+  { icon: 'fa-solid fa-seedling', title: 'Aprendizaje Continuo' },
 ];
 
+
 const proyectos = [
-    { 
-        link: 'https://mariogame-js.netlify.app/',
-        imagen: 'assets/img/marioGame.png',
-        titulo: 'Juego de Mario',
-        tecnologias: 'HTML - JS - CSS'
-    },
-    { 
-        link: 'https://luis-angel-santos.github.io/Blackjack/',
-        imagen: 'assets/img/blackjack.png',
-        titulo: 'BlackJack',
-        tecnologias: 'HTML - JS - CSS'
-    },
-    { 
-        link: 'https://luis-angel-santos.github.io/PeliculasApp/home',
-        imagen: 'assets/img/peliculas.png',
-        titulo: 'App de Películas',
-        tecnologias: 'Desarrollada usando una API'
-    },
-    { 
-        link: 'https://luis-angel-santos.github.io/appClima/',
-        imagen: 'assets/img/clima.png',
-        titulo: 'App del Clima',
-        tecnologias: 'Desarrollada usando una API'
-    },
-    { 
-        link: 'https://luis-angel-santos.github.io/Pagina-de-Comida/',
-        imagen: 'assets/img/comida.png',
-        titulo: 'Página de Restaurante',
-        tecnologias: 'HTML - CSS - JS'
-    },
     { 
         link: 'https://fisio.netlify.app/',
         imagen: 'assets/img/fisio.png',
@@ -87,31 +82,71 @@ const proyectos = [
         tecnologias: 'Angular v15 - TypeScript - NodeJS - PWA'
     },
     { 
-        link: 'https://jokes-app-lasi.netlify.app/',
-        imagen: 'assets/img/jokes.png',
-        titulo: 'Jokes App',
-        tecnologias: 'Angular v15 - TypeScript - NodeJS - API'
+        link: 'https://ticketfactura.com.mx',
+        imagen: 'assets/img/facturacion_pg.png',
+        titulo: 'Facturación Electrónica Grupo Empresarial PG',
+        tecnologias: 'Angular v15 - TypeScript - Java'
     },
     { 
-        link: 'https://heroes-lasi.netlify.app/',
-        imagen: 'assets/img/heroes.png',
-        titulo: 'Héroes App',
-        tecnologias: 'Angular v15 - TypeScript - NodeJS - PWA'
+        link: 'https://mifactura.vgbizgroup.com/',
+        imagen: 'assets/img/v&g.png',
+        titulo: 'Facturación Electrónica V&G Business Group',
+        tecnologias: 'Angular v15 - TypeScript - Java'
     },
     { 
-        link: 'https://trivia-lasi.netlify.app/',
-        imagen: 'assets/img/trivia.png',
-        titulo: 'Trivia App',
-        tecnologias: 'Angular v15 - TypeScript - NodeJS - PWA'
+        link: 'https://login.wiprecargas.com/',
+        imagen: 'assets/img/wiprecargas.png',
+        titulo: 'WipRecargas Web',
+        tecnologias: 'Java - JSF - PrimeFaces'
     },
     { 
-        link: 'https://sistema-venta-lasi.netlify.app/',
-        imagen: 'assets/img/sistemaVentas.png',
-        titulo: 'Sistema de Ventas',
-        tecnologias: 'Angular v15 - C# - MySQL - PWA'
+        link: 'https://play.google.com/store/apps/details?id=com.wiprecargas.mobile.v2.activity&hl=es_MX',
+        imagen: 'assets/img/wip_app.png',
+        titulo: 'WipRecargas Android',
+        tecnologias: 'Java - Android Studio - XML'
+    },
+    { 
+        link: 'https://luis-angel-santos.github.io/PeliculasApp/home',
+        imagen: 'assets/img/peliculas.png',
+        titulo: 'Repertorio de Películas',
+        tecnologias: 'Desarrollada usando una API'
+    },
+    { 
+        link: 'https://luis-angel-santos.github.io/appClima/',
+        imagen: 'assets/img/clima.png',
+        titulo: 'App del Clima',
+        tecnologias: 'Desarrollada usando una API'
     }
 ];
 
+function typeEffect() {
+    const fraseActual = frases[fraseIndex];
+
+    if (!borrando) {
+      // Escribiendo
+      typingElement.textContent = fraseActual.substring(0, letraIndex + 1);
+      letraIndex++;
+
+      if (letraIndex === fraseActual.length) {
+        // Pausa antes de borrar
+        borrando = true;
+        setTimeout(typeEffect, 2000);
+        return;
+      }
+    } else {
+      // Borrando
+      typingElement.textContent = fraseActual.substring(0, letraIndex - 1);
+      letraIndex--;
+
+      if (letraIndex === 0) {
+        borrando = false;
+        fraseIndex = (fraseIndex + 1) % frases.length;
+      }
+    }
+
+    const velocidad = borrando ? 30 : 100; // velocidad escribir/borrar
+    setTimeout(typeEffect, velocidad);
+  }
 
 function seleccionar(){
     //oculto el menu una vez que selecciono una opcion
@@ -247,7 +282,7 @@ function contacto(){
     var tema = document.getElementsByClassName('datosContacto')[3].value;
     var mensaje = document.getElementsByClassName('datosContacto')[4].value;
 
-    window.location.href = `mailto:santosangel748@gmail.com?Subject=${tema}&body=Hola%20que%20tal%20soy%20${nombre},%20me%20gustaria%20decirte%20lo%20siguiente%20${mensaje}.
+    window.location.href = `mailto:s.angedev@gmail.com?Subject=${tema}&body=Hola%20que%20tal%20soy%20${nombre},%20me%20gustaria%20decirte%20lo%20siguiente%20${mensaje}.
         %20Aquí%20te%20dejo%20mis%20datos%20de%20contacto:%20Telefono-->%20${telefono}%20y%20Correo-->%20${correo}.
         Un%20saludo,%20${nombre}`
 }
@@ -257,5 +292,6 @@ generarHabilidadesTecnicas();
 generarIntereses();
 generarHabilidadesBlandas();
 generarProyectos();
+typeEffect();
 
 
